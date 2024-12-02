d9b6a18c74a565032b8337696f41ccf7c82c8758
BIG DON <BIG DON@MSI>
2024-12-02 09:05:02 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CHALLENGE_3_1
+{
+    public class PalindromeChecker
+    {
+        public static bool IsPalindrome(string s)
+        {
+            //compare the sting with its reversed version
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedString = new string(charArray);
+
+            return s == reversedString;
+        }
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine(IsPalindrome("eye"));  //output: True
+            Console.WriteLine(IsPalindrome("home")); //output: False
+
+        }
+    }
+}
